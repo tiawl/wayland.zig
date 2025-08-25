e7248f196605f768c77444c41eba1e09b3eb8592
tiawl <pabtomas@protonmail.com>
2025-08-25 14:16:59 +0200

    zig 0.15.1: typo

--- build.zig
+++ build.zig
@@ -241,7 +241,7 @@ pub fn build(builder: *std.Build) !void {
 
     const lib = builder.addLibrary(.{
         .name = "wayland",
-        .root_module = std.Builder.Module.create(builder, .{
+        .root_module = std.Build.Module.create(builder, .{
             .root_source_file = builder.addWriteFiles().add("empty.c", ""),
             .target = target,
             .optimize = optimize,
